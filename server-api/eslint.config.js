cde1bcb9e17b24548223cec8186240f18dce4261
Neil Smyth <neil@thesmyths.eu>
2025-01-24 17:43:28 +0100

    tidy up the lint rules

--- server-api/eslint.config.js
+++ server-api/eslint.config.js
@@ -2,7 +2,6 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-const env = (prod, dev) => (process.env.NODE_ENV === 'production' ? prod : dev);
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
@@ -10,20 +9,12 @@ export default [
   {
     rules: {
       quotes: ['error', 'single', { avoidEscape: true }],
-      'no-console': env(1, 0),
-      'no-debugger': env(1, 0),
       // 'jest/no-focused-tests': 'warn',
       // 'jest/no-identical-title': 'error',
       '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        env(2, 1),
-        {
-          argsIgnorePattern: '^_',
-        },
-      ],
       'no-multiple-empty-lines': 'error',
     },
   },
